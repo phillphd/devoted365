be17872706f98477165d81aec80ffa86a1c7ade7
patrick <patrick@whiteboard.is>
2016-10-28 19:58:54 -0400

    moving styles / scripts to functions

--- wp-content/themes/launchframe/functions.php
+++ wp-content/themes/launchframe/functions.php
@@ -38,11 +38,14 @@ class LaunchframeSite extends TimberSite {
 
   	function register_stylesheets() {
     	global $package_version;
+    	wp_enqueue_style( 'lato-style', 'https://fonts.googleapis.com/css?family=Lato', true, $package_version );
     	wp_enqueue_style( 'application-style', get_template_directory_uri() . '/assets/dist/css/application.min.css', true, $package_version );
   	}
 
   	function register_scripts() {
   		global $package_version;
+  		wp_enqueue_script( 'jquery-js', 'https://code.jquery.com/jquery-3.1.1.min.js', true, $package_version );
+  		wp_enqueue_script( 'modernizr-js', 'https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js', true, $package_version );
     	wp_enqueue_script( 'application-js', get_template_directory_uri() . '/assets/dist/js/script.min.js', true, $package_version );
   	}
 
